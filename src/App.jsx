ea87903b882ce63271ec3331bea4e3495fc92ac3
GPplay <gybramdali2002@hotmail.com>
2024-08-24 13:17:33 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// Supports weights 100-900
+import '@fontsource-variable/saira';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-8xl text-blue-300'>hola mundo</h1>
+    </>
+  )
+}
+
+export default App
